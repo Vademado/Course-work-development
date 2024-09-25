397678fea19ef1ae3a40e3a3a49a00c4fd106007
Vademado <VademadoIU8@yandex.ru>
2024-09-26 02:52:31 +0300

    add some functional

--- CFG.py
+++ CFG.py
@@ -1,2 +1,6 @@
 class CFG:
-    pass
+    def __init__(self, number_vertices, number_edges):
+        pass
+
+    def __creating_json(self):
+        pass
--- sources.py
+++ sources.py
@@ -1,11 +1,12 @@
 from random import randint
 
-NUMBER_OF_VERTICES = 0
-RANGE_OF_NUMBER_OF_EDGES = 0
+INPUT_DATA = None
+NUMBER_OF_VERTICES = None
+RANGE_OF_NUMBER_OF_EDGES = None
 
 
 def reading_data():
-    global NUMBER_OF_VERTICES, RANGE_OF_NUMBER_OF_EDGES
+    global INPUT_DATA, NUMBER_OF_VERTICES, RANGE_OF_NUMBER_OF_EDGES
     print("Введите количество вершин:", end=' ')
     NUMBER_OF_VERTICES = int(input())
     RANGE_OF_NUMBER_OF_EDGES = randint(NUMBER_OF_VERTICES // 5 * (NUMBER_OF_VERTICES - 1),
