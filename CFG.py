f7cbfd6f5b7655fd5c98dcb431a156cedfba1355
Вадим <VademadoIU8@yandex.ru>
2024-09-30 12:51:41 +0300

    intermediate commit

--- CFG.py
+++ CFG.py
@@ -1,6 +1,8 @@
 class CFG:
     def __init__(self, number_vertices, number_edges):
-        pass
+        self.__number_vertices = number_vertices
+        self.__number_edges = number_edges
+        self.__creating_json()
 
     def __creating_json(self):
         pass
