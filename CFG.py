ab02b4a2009792f79ba6ba359d6bf44039f16015
Вадим <VademadoIU8@yandex.ru>
2024-10-02 03:00:50 +0300

    add enums

--- CFG.py
+++ CFG.py
@@ -1,8 +1,53 @@
+from sources import ComparisonOperators, Operations
+
+
+class BaseBlock:
+    id = 0
+
+    def __int__(self, operations, input_data):
+        self.__id = BaseBlock.id + 1
+        BaseBlock.id += 1
+        self.operations = [(Operations.ADDITION, 9)]
+        self.edges = []
+
+    def __block_execution(self, input_data):
+        for operation in self.operations:
+            match operation[0]:
+                case Operations.ADDITION: input_data += operation[1]
+            match operation[0]:
+                case Operations.SUBTRACTION: input_data -= operation[1]
+            match operation[0]:
+                case Operations.MULTIPLICATION: input_data *= operation[1]
+            match operation[0]:
+                case Operations.DIVISION: input_data //= operation[1]
+            match operation[0]:
+                case Operations.EXPONENTIATION: input_data **= operation[1]
+            match operation[0]:
+                case Operations.DIVISION_BY_MODULUS: input_data %= operation[1]
+
+    def add_edge(self, edge):
+        pass
+
+
+class Edge:
+    def __int__(self, from_vertex, to_vertex, condition):
+        self.from_vertex = from_vertex
+        self.to_vertex = to_vertex
+
+
 class CFG:
-    def __init__(self, number_vertices, number_edges):
+    def __init__(self, number_vertices, number_edges, input_data):
         self.__number_vertices = number_vertices
         self.__number_edges = number_edges
-        self.__creating_json()
+        self.adjacency_dictionary = dict()
+        self.existing_vertices = set()
+        self.start_vertex = None
 
-    def __creating_json(self):
-        pass
+    def __generate_cfg(self):
+        for i in range(self.__number_edges):
+            from_vertex = randint(0, self.__number_edges + 1)
+            to_vertex = randint(0, self.__number_edges + 1)
+
+            # if from_vertex in self.existing_vertices:
+            # else:
+            #     newVertex = BaseBlock('+5*2//3%56')
--- sources.py
+++ sources.py
@@ -1,13 +1,34 @@
 from random import randint
+from enum import Enum
 
 INPUT_DATA = None
 NUMBER_OF_VERTICES = None
 RANGE_OF_NUMBER_OF_EDGES = None
 
 
+class ComparisonOperators(Enum):
+    EQUALITY: 0               # ==
+    INEQUALITY: 1             # !=
+    LESS_THAN: 2              # <
+    GREATER_THAN: 3           # >
+    LESS_THAN_OR_EQUAL: 4     # <=
+    GREATER_THAN_OR_EQUAL: 5  # >=
+    COMPARABLE_MODULO: 6      # % ==
+    INCOMPARABLY_MODULO: 7    # % !=
+
+
+class Operations(Enum):
+    ADDITION: 0               # +=
+    SUBTRACTION: 1            # -=
+    MULTIPLICATION: 2         # *=
+    DIVISION: 3               # //=
+    EXPONENTIATION: 4         # **=
+    DIVISION_BY_MODULUS: 5    # %=
+
+
 def reading_data():
     global INPUT_DATA, NUMBER_OF_VERTICES, RANGE_OF_NUMBER_OF_EDGES
     print("Введите количество вершин:", end=' ')
     NUMBER_OF_VERTICES = int(input())
-    RANGE_OF_NUMBER_OF_EDGES = randint(NUMBER_OF_VERTICES // 5 * (NUMBER_OF_VERTICES - 1),
-                                       (NUMBER_OF_VERTICES - 1) ** 2 + 1)
+    RANGE_OF_NUMBER_OF_EDGES = randint(1.3 * NUMBER_OF_VERTICES,
+                                       2 * (NUMBER_OF_VERTICES - 1))
