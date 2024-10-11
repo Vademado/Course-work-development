6c675b3518a2b65564fa3443a0e30909a5637686
Vademado <VademadoIU8@yandex.ru>
2024-10-12 00:41:23 +0300

    version 1.0

--- CFG.py
+++ CFG.py
@@ -1,5 +1,5 @@
 from sources import ComparisonOperators, Operations
-from random import randint
+from random import randint, choice
 
 
 # ОБРАБОТАТЬ СИТУЦИИ, КОГДА В ПРОЦЕССЕ ВЫПОЛНЕНИЯ БАЗОВОГО БЛОКА INPUT_DATA ОБРАЩАЕТСЯ В НОЛЬ
@@ -9,13 +9,22 @@ class BaseBlock:
 
     def __init__(self, operations, input_data=None):
         self.id = BaseBlock.id
-        print(self.id)
         BaseBlock.id += 1
         self.__operations = operations
         self.__edges = []
 
     # ОПРЕДЕЛИТЬ ПОВЕДЕНИЕ ПРИ operation[1] == 0
     def __block_execution(self, input_data):
+        """Returns the data converted in the base block.
+
+                    :param input_data: arg1
+                    :type input_data: int
+
+                    :raises ValueError: if division by zero is performed
+
+                    :rtype: int
+                    :return: the result of processing the input data in the base block
+                    """
         for operation in self.__operations:
             match operation[0]:
                 case Operations.ADDITION:
@@ -36,8 +45,34 @@ class BaseBlock:
                         input_data %= operation[1]
                     else:
                         raise ValueError("Division by zero is not allowed.")
+                case Operations.BIT_SHIFT_TO_LEFT:
+                    input_data <<= operation[1]
+                case Operations.BIT_SHIFT_TO_RIGHT:
+                    input_data >>= operation[1]
+                case Operations.BITWISE_OR:
+                    input_data |= operation[1]
+                case Operations.BITWISE_EXCLUSIVE_OR:
+                    input_data ^= operation[1]
+                case Operations.BITWISE_AND:
+                    input_data &= operation[1]
+                case Operations.BIT_INVERSION:
+                    input_data = ~input_data
 
     def get_operations(self):
+        """Return sum of multiplication of all arguments.
+
+            :param a: arg1
+            :type a: int
+            :param b: arg2
+            :type b: int
+            :param c: arg3, defaults to 0
+            :type c: int, optional
+
+            :raises ValueError: if arg1 is equal to arg2
+
+            :rtype: int
+            :return: multiplication of all arguments
+            """
         return self.__operations
 
     def get_edges(self):
@@ -75,17 +110,26 @@ class CFG:
         for i in range(self.__number_base_blocks):
             number_operations = randint(1, len(Operations))
             # ОПРЕДЕЛИТЬ ЧИСЛЕННЫЕ ЗНАЧЕНИЕ ОПЕРАЦИИ (ПРЕДЕЛЫ)
-            operations = [(Operations(randint(0, len(Operations) - 1)), randint(-100, 100)) for _ in
+            operations = [(Operations(randint(0, len(Operations) - 1)), randint(0, 10)) for _ in
                           range(number_operations)]
-            # print(operations)
-            self.__dictionary_base_blocks[i] = BaseBlock(operations, self.__input_data if not i else None)
-        # print(self.dictionary_base_blocks)
+            self.__dictionary_base_blocks[i] = BaseBlock(operations, None if i else self.__input_data)
 
-        for _ in range(self.__number_edges):
-            while True:
-                from_base_block = randint(0, self.__number_base_blocks - 1)
-                to_base_block = randint(0, self.__number_base_blocks - 1)
-                if len(self.__dictionary_base_blocks[from_base_block].get_edges()) < 2 and  to_base_block: break
+        self.__base_blocks_related_with_initial_base_block = {0}
+        self.__base_blocks_unrelated_with_initial_base_block= set(range(1, self.__number_base_blocks))
+
+        for i in range(self.__number_edges):
+            if len(self.__base_blocks_unrelated_with_initial_base_block) == self.__number_edges - i:
+                to_base_block = choice(list(self.__base_blocks_unrelated_with_initial_base_block))
+                while True:
+                    from_base_block = choice(list(self.__base_blocks_related_with_initial_base_block))
+                    if len(self.__dictionary_base_blocks[from_base_block].get_edges()) < 2: break
+                self.dfs(to_base_block)
+            else:
+                while True:
+                    from_base_block = randint(0, self.__number_base_blocks - 1)
+                    to_base_block = randint(0, self.__number_base_blocks - 1)
+                    if len(self.__dictionary_base_blocks[from_base_block].get_edges()) < 2 and to_base_block: break
+
             if self.__dictionary_base_blocks[from_base_block].get_edges():
                 first_edge_condition = self.__dictionary_base_blocks[from_base_block].get_edges()[0].get_condition()
                 match first_edge_condition[0]:
@@ -113,8 +157,6 @@ class CFG:
                     case ComparisonOperators.INCOMPARABLY_MODULO:
                         new_condition = (
                             ComparisonOperators.COMPARABLE_MODULO, first_edge_condition[1], first_edge_condition[2])
-                self.__dictionary_base_blocks[from_base_block].add_edge(
-                    Edge(from_base_block, to_base_block, new_condition))
             else:
                 comparison_operator = ComparisonOperators(randint(0, 7))
                 if comparison_operator == ComparisonOperators.COMPARABLE_MODULO or comparison_operator == ComparisonOperators.INCOMPARABLY_MODULO:
@@ -125,8 +167,17 @@ class CFG:
                     module = None
                     # ОПРЕДЕЛИТЬ ЧИСЛЕННЫЕ ЗНАЧЕНИЕ ОПЕРАЦИИ (ПРЕДЕЛЫ)
                     value_for_comparison = randint(-100, 100)
-                self.__dictionary_base_blocks[from_base_block].add_edge(
-                    Edge(from_base_block, to_base_block, (comparison_operator, module, value_for_comparison)))
+                new_condition = (comparison_operator, module, value_for_comparison)
+            self.__dictionary_base_blocks[from_base_block].add_edge(Edge(from_base_block, to_base_block, new_condition))
+
+    def dfs(self, id_base_block, visited_base_blocks=None):
+        if visited_base_blocks is None: visited_base_blocks = [False] * self.__number_base_blocks
+        visited_base_blocks[id_base_block] = True
+        self.__base_blocks_related_with_initial_base_block.add(id_base_block)
+        self.__base_blocks_unrelated_with_initial_base_block.discard(id_base_block)
+        for edge in self.__dictionary_base_blocks[id_base_block].get_edges():
+            if not visited_base_blocks[edge.get_to_base_block_id()]:
+                self.dfs(edge.get_to_base_block_id(), visited_base_blocks)
 
     def get_dictionary_base_blocks(self):
         return self.__dictionary_base_blocks
--- sources.py
+++ sources.py
@@ -10,36 +10,42 @@ class Constants:
 
 
 class ComparisonOperators(Enum):
-    EQUALITY = 0  # ==
-    INEQUALITY = 1  # !=
-    LESS_THAN = 2  # <
-    GREATER_THAN = 3  # >
-    LESS_THAN_OR_EQUAL = 4  # <=
-    GREATER_THAN_OR_EQUAL = 5  # >=
-    COMPARABLE_MODULO = 6  # % ==
-    INCOMPARABLY_MODULO = 7  # % !=
+    EQUALITY = 0                # ==
+    INEQUALITY = 1              # !=
+    LESS_THAN = 2               # <
+    GREATER_THAN = 3            # >
+    LESS_THAN_OR_EQUAL = 4      # <=
+    GREATER_THAN_OR_EQUAL = 5   # >=
+    COMPARABLE_MODULO = 6       # % ==
+    INCOMPARABLY_MODULO = 7     # % !=
 
 
 class Operations(Enum):
-    ADDITION = 0  # +=
-    SUBTRACTION = 1  # -=
-    MULTIPLICATION = 2  # *=
-    DIVISION = 3  # //=
-    EXPONENTIATION = 4  # **=
-    DIVISION_BY_MODULUS = 5  # %=
+    ADDITION = 0                # +=
+    SUBTRACTION = 1             # -=
+    MULTIPLICATION = 2          # *=
+    DIVISION = 3                # //=
+    EXPONENTIATION = 4          # **=
+    DIVISION_BY_MODULUS = 5     # %=
+    BIT_SHIFT_TO_LEFT = 6       # <<
+    BIT_SHIFT_TO_RIGHT = 7      # >>
+    BITWISE_OR = 8              # x|y
+    BITWISE_EXCLUSIVE_OR = 9    # x^y
+    BITWISE_AND = 10            # x&y
+    BIT_INVERSION = 11          # ~x
 
 
 def read_data():
     print("Введите количество вершин:", end=' ')
     Constants.NUMBER_OF_BASE_BLOCKS = int(input())
-    print(int(1.3 * Constants.NUMBER_OF_BASE_BLOCKS), 2 * (Constants.NUMBER_OF_BASE_BLOCKS - 1))
     Constants.NUMBER_OF_EDGES = randint(int(1.3 * Constants.NUMBER_OF_BASE_BLOCKS),
                                         2 * (Constants.NUMBER_OF_BASE_BLOCKS - 1))
 
 
 def operations_in_base_block_convert_to_string(base_block):
     string_operations_in_base_block = ""
-    if base_block.id == 0 : string_operations_in_base_block += "INITIAL BLOCK\n"
+    if base_block.id == 0 : string_operations_in_base_block += "INITIAL BLOCK\n\n"
+    else: string_operations_in_base_block += f"{base_block.id}\n\n"
     for operation in base_block.get_operations():
         match operation[0]:
             case Operations.ADDITION:
@@ -54,6 +60,18 @@ def operations_in_base_block_convert_to_string(base_block):
                 string_operations_in_base_block += f"X **= {operation[1]}\n"
             case Operations.DIVISION_BY_MODULUS:
                 string_operations_in_base_block += f"X %= {operation[1]}\n"
+            case Operations.BIT_SHIFT_TO_LEFT:
+                string_operations_in_base_block += f"X << {operation[1]}\n"
+            case Operations.BIT_SHIFT_TO_RIGHT:
+                string_operations_in_base_block += f"X >> {operation[1]}\n"
+            case Operations.BITWISE_OR:
+                string_operations_in_base_block += f"X | {operation[1]}\n"
+            case Operations.BITWISE_EXCLUSIVE_OR:
+                string_operations_in_base_block += f"X ^ {operation[1]}\n"
+            case Operations.BITWISE_AND:
+                string_operations_in_base_block += f"X & {operation[1]}\n"
+            case Operations.BIT_INVERSION:
+                string_operations_in_base_block += "~X\n"
     return string_operations_in_base_block
 
 
@@ -91,5 +109,5 @@ def visualize_cfg(cfg):
             condition_in_edge = condition_in_edge_convert_to_string(edge)
             dot.edge(str(edge.get_from_base_block_id()), str(edge.get_to_base_block_id()), condition_in_edge)
 
-    dot.render('Control-flow graph', format='png')
-    # dot.render('Control-flow graph.pdf', view=True)
+    #dot.render('Control-flow graph', format='png')
+    dot.render('Control-flow graph', view=True)
